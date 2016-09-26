8da55cbe5401f497a30525e783db539478d30b0b
Maulana Yusup Abdullah <adeneling@gmail.com>
2016-09-26 15:58:20 +0700

    update

--- resources/views/frontend/pages/login.blade.php
+++ resources/views/frontend/pages/login.blade.php
@@ -26,7 +26,10 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading"><center>Best Customer Service Awards 2016 | Telkomsel</center></div>
+                    <center>
+                        <h1>Best Customer Service Awards 2016<br>Telkomsel</h1>
+                    </center>
+                    <hr>
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                             {{ csrf_field() }}
