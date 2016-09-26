6fd2c1362bbf56eecc81dde4b1bf4a2ca6b97919
Maulana Yusup Abdullah <adeneling@gmail.com>
2016-09-26 19:24:18 +0700

    update

--- resources/views/participant/pages/profile.blade.php
+++ resources/views/participant/pages/profile.blade.php
@@ -3,15 +3,7 @@
 @section('title'){{ Auth::user()->name }}
 @stop
 @section('content')
-<div class="row">
-    <div class="col-md-12 col-sm-12 col-xs-12">
-        <div class="x_panel">
-            <div class="x_content">
-        		<center><a href="" class="btn btn-danger btn-lg"><h4>Upload File</h4></a></center>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 <br>
 
--- resources/views/participant/pages/upload-file.blade.php
+++ resources/views/participant/pages/upload-file.blade.php
@@ -3,5 +3,13 @@
 @section('title')Upload File
 @stop
 @section('content')
-
+<div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+            <div class="x_content">
+        		<center><a href="" class="btn btn-danger btn-lg"><h4>Upload File</h4></a></center>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
