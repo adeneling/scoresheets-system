e11797ed82fbe6e28d303f16b770c2cd6bddd0b8
Maulana Yusup Abdullah <adeneling@gmail.com>
2016-10-02 09:44:31 +0700

    menu_fixed

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -35,7 +35,7 @@
 <body class="nav-md">
   <div class="container body">
     <div class="main_container">
-      <div class="col-md-3 left_col">
+      <div class="col-md-3 left_col menu_fixed">
         <div class="left_col scroll-view">
           <div class="clearfix"></div>
           <!-- menu profile quick info -->
