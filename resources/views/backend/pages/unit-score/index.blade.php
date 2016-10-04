8bf8c034a740502fc43d0639afbe46fc03bafb88
Maulana Yusup Abdullah <adeneling@gmail.com>
2016-10-05 06:41:37 +0700

    update lable

--- resources/views/backend/pages/unit-score/index.blade.php
+++ resources/views/backend/pages/unit-score/index.blade.php
@@ -93,80 +93,84 @@
 						</tr>
 					</thead>
 					<tbody>
+						{{-- TIER 1 --}}
 						<tr>
-							<th class="red"><font color="white">BEST GRAPARI</font></th>
-							<td>{{ $nilaiCategori1[0]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori1[1]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori1[2]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori1[3]==1 ? 'JUARA 1' : ''}}</td>
+							<th class="red"><font color="white">THE BEST GRAPARI</font></th>
+							<td>{{ $nilaiCategori1[0]==1 ? 'JUARA 1 (' .$resultCategori1[0].')' : ''}}</td>
+							<td>{{ $nilaiCategori1[1]==1 ? 'JUARA 1 (' .$resultCategori1[1].')' : ''}}</td>
+							<td>{{ $nilaiCategori1[2]==1 ? 'JUARA 1 (' .$resultCategori1[2].')' : ''}}</td>
+							<td>{{ $nilaiCategori1[3]==1 ? 'JUARA 1 (' .$resultCategori1[3].')' : ''}}</td>
 						</tr>
 						<tr>
-							<th class="red"><font color="white">BEST SERVICE DIFFERENTIATION CE ACROSS CHANNEL</font></th>
-							<td>{{ $nilaiCategori2[0]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori2[1]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori2[2]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori2[3]==1 ? 'JUARA 1' : ''}}</td>
+							<th class="red"><font color="white">THE BEST GRAPARI MITRA</font></th>
+							<td>{{ $nilaiCategori3[0]==1 ? 'JUARA 1 (' .$resultCategori3[0].')' : ''}}</td>
+							<td>{{ $nilaiCategori3[1]==1 ? 'JUARA 1 (' .$resultCategori3[1].')' : ''}}</td>
+							<td>{{ $nilaiCategori3[2]==1 ? 'JUARA 1 (' .$resultCategori3[2].')' : ''}}</td>
+							<td>{{ $nilaiCategori3[3]==1 ? 'JUARA 1 (' .$resultCategori3[3].')' : ''}}</td>
 						</tr>
 						<tr>
-							<th class="red"><font color="white">BEST GRAPARI MITRA</font></th>
-							<td>{{ $nilaiCategori3[0]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori3[1]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori3[2]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori3[3]==1 ? 'JUARA 1' : ''}}</td>
+							<th class="red"><font color="white">THE BEST SERVICE DIFFERENTIATION CE ACROSS CHANNEL</font></th>
+							<td>{{ $nilaiCategori2[0]==1 ? 'JUARA 1 (' .$resultCategori2[0].')' : ''}}</td>
+							<td>{{ $nilaiCategori2[1]==1 ? 'JUARA 1 (' .$resultCategori2[1].')' : ''}}</td>
+							<td>{{ $nilaiCategori2[2]==1 ? 'JUARA 1 (' .$resultCategori2[2].')' : ''}}</td>
+							<td>{{ $nilaiCategori2[3]==1 ? 'JUARA 1 (' .$resultCategori2[3].')' : ''}}</td>
 						</tr>
 						<tr>
-							<th class="red"><font color="white">BEST OUTBOUND CALL CENTER</font></th>
-							<td>{{ $nilaiCategori6[0]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori6[1]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori6[2]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori6[3]==1 ? 'JUARA 1' : ''}}</td>
+							<th class="red"><font color="white">THE BEST INBOUND CALL CENTER</font></th>
+							<td>{{ $nilaiCategori7[0]==1 ? 'JUARA 1 (' .$resultCategori7[0].')' : ''}}</td>
+							<td>{{ $nilaiCategori7[1]==1 ? 'JUARA 1 (' .$resultCategori7[1].')' : ''}}</td>
+							<td>{{ $nilaiCategori7[2]==1 ? 'JUARA 1 (' .$resultCategori7[2].')' : ''}}</td>
+							<td>{{ $nilaiCategori7[3]==1 ? 'JUARA 1 (' .$resultCategori7[3].')' : ''}}</td>
 						</tr>
 						<tr>
-							<th class="red"><font color="white">BEST INBOUND CALL CENTER</font></th>
-							<td>{{ $nilaiCategori7[0]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori7[1]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori7[2]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori7[3]==1 ? 'JUARA 1' : ''}}</td>
+							<th class="red"><font color="white">THE BEST OUTBOUND CALL CENTER</font></th>
+							<td>{{ $nilaiCategori6[0]==1 ? 'JUARA 1 (' .$resultCategori6[0].')' : ''}}</td>
+							<td>{{ $nilaiCategori6[1]==1 ? 'JUARA 1 (' .$resultCategori6[1].')' : ''}}</td>
+							<td>{{ $nilaiCategori6[2]==1 ? 'JUARA 1 (' .$resultCategori6[2].')' : ''}}</td>
+							<td>{{ $nilaiCategori6[3]==1 ? 'JUARA 1 (' .$resultCategori6[3].')' : ''}}</td>
+						</tr>			
+
+						{{-- TIER 2 --}}
+						<tr>
+							<th class="red"><font color="white">THE BEST FRONT OFFICE SUPPORT</font></th>
+							<td>{{ $nilaiCategori4[0]==1 ? 'JUARA 1 (' .$resultCategori4[0].')' : ''}}</td>
+							<td>{{ $nilaiCategori4[1]==1 ? 'JUARA 1 (' .$resultCategori4[1].')' : ''}}</td>
+							<td>{{ $nilaiCategori4[2]==1 ? 'JUARA 1 (' .$resultCategori4[2].')' : ''}}</td>
+							<td>{{ $nilaiCategori4[3]==1 ? 'JUARA 1 (' .$resultCategori4[3].')' : ''}}</td>
+						</tr>
+						<tr>
+							<th class="red"><font color="white">THE BEST COMPLAINT HANDLING OFFICER</font></th>
+							<td>{{ $nilaiCategori8[0]==1 ? 'JUARA 1 (' .$resultCategori8[0].')' : ''}}</td>
+							<td>{{ $nilaiCategori8[1]==1 ? 'JUARA 1 (' .$resultCategori8[1].')' : ''}}</td>
+							<td>{{ $nilaiCategori8[2]==1 ? 'JUARA 1 (' .$resultCategori8[2].')' : ''}}</td>
+							<td>{{ $nilaiCategori8[3]==1 ? 'JUARA 1 (' .$resultCategori8[3].')' : ''}}</td>
+						</tr>
+						<tr>
+							<th class="red"><font color="white">THE BEST TRAINED CALL CENTER</font></th>
+							<td>{{ $nilaiCategori9[0]==1 ? 'JUARA 1 (' .$resultCategori9[0].')' : ''}}</td>
+							<td>{{ $nilaiCategori9[1]==1 ? 'JUARA 1 (' .$resultCategori9[1].')' : ''}}</td>
+							<td>{{ $nilaiCategori9[2]==1 ? 'JUARA 1 (' .$resultCategori9[2].')' : ''}}</td>
+							<td>{{ $nilaiCategori9[3]==1 ? 'JUARA 1 (' .$resultCategori9[3].')' : ''}}</td>
 						</tr>
 
-
+						{{-- TIER 3 --}}
 						<tr>
-							<th class="red"><font color="white">BEST FRONT OFFICE SUPPORT</font></th>
-							<td>{{ $nilaiCategori4[0]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori4[1]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori4[2]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori4[3]==1 ? 'JUARA 1' : ''}}</td>
-						</tr>
-						<tr>
-							<th class="red"><font color="white">BEST COMPLAINT HANDLING</font></th>
-							<td>{{ $nilaiCategori8[0]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori8[1]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori8[2]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori8[3]==1 ? 'JUARA 1' : ''}}</td>
-						</tr>
-						<tr>
-							<th class="red"><font color="white">BEST TRAINED</font></th>
-							<td>{{ $nilaiCategori9[0]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori9[1]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori9[2]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori9[3]==1 ? 'JUARA 1' : ''}}</td>
+							<th class="red"><font color="white">THE BEST BILLING COLLECTION AREA</font></th>
+							<td>{{ $nilaiCategori10[0]==1 ? 'JUARA 1 (' .$resultCategori10[0].')' : ''}}</td>
+							<td>{{ $nilaiCategori10[1]==1 ? 'JUARA 1 (' .$resultCategori10[1].')' : ''}}</td>
+							<td>{{ $nilaiCategori10[2]==1 ? 'JUARA 1 (' .$resultCategori10[2].')' : ''}}</td>
+							<td>{{ $nilaiCategori10[3]==1 ? 'JUARA 1 (' .$resultCategori10[3].')' : ''}}</td>
 						</tr>
 
+						{{-- TIER 4 --}}
 						<tr>
-							<th class="red"><font color="white">BEST BACK END SUPPORT</font></th>
-							<td>{{ $nilaiCategori5[0]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori5[1]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori5[2]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori5[3]==1 ? 'JUARA 1' : ''}}</td>
+							<th class="red"><font color="white">THE BEST BACK END SUPPORT</font></th>
+							<td>{{ $nilaiCategori5[0]==1 ? 'JUARA 1 (' .$resultCategori5[0].')' : ''}}</td>
+							<td>{{ $nilaiCategori5[1]==1 ? 'JUARA 1 (' .$resultCategori5[1].')' : ''}}</td>
+							<td>{{ $nilaiCategori5[2]==1 ? 'JUARA 1 (' .$resultCategori5[2].')' : ''}}</td>
+							<td>{{ $nilaiCategori5[3]==1 ? 'JUARA 1 (' .$resultCategori5[3].')' : ''}}</td>
 						</tr>						
 						
-						<tr>
-							<th class="red"><font color="white">BEST BILLING COLLECTION</font></th>
-							<td>{{ $nilaiCategori10[0]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori10[1]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori10[2]==1 ? 'JUARA 1' : ''}}</td>
-							<td>{{ $nilaiCategori10[3]==1 ? 'JUARA 1' : ''}}</td>
-						</tr>
 						<tr >
 							<th colspan="1">TOTAL</th>
 							<th>{{ $bestAreaResult[0] }}</th>
@@ -215,28 +219,28 @@
 					</thead>
 					<tbody>
 						<tr>
-							<th class="red"><font color="white">BEST OUTBOUND CALL CENTER</font></th>
-							<td>{{ $nilaiCategori6[0]==1 ? 'JUARA 1 (' .$resultCategori6[0].')' : ''}}</td>
-							<td>{{ $nilaiCategori6[1]==1 ? 'JUARA 1 (' .$resultCategori6[1].')' : ''}}</td>
-							<td>{{ $nilaiCategori6[2]==1 ? 'JUARA 1 (' .$resultCategori6[2].')' : ''}}</td>
-							<td>{{ $nilaiCategori6[3]==1 ? 'JUARA 1 (' .$resultCategori6[3].')' : ''}}</td>
-						</tr>
-						<tr>
-							<th class="red"><font color="white">BEST INBOUND CALL CENTER</font></th>
+							<th class="red"><font color="white">THE BEST INBOUND CALL CENTER</font></th>
 							<td>{{ $nilaiCategori7[0]==1 ? 'JUARA 1 (' .$resultCategori7[0].')' : ''}}</td>
 							<td>{{ $nilaiCategori7[1]==1 ? 'JUARA 1 (' .$resultCategori7[1].')' : ''}}</td>
 							<td>{{ $nilaiCategori7[2]==1 ? 'JUARA 1 (' .$resultCategori7[2].')' : ''}}</td>
 							<td>{{ $nilaiCategori7[3]==1 ? 'JUARA 1 (' .$resultCategori7[3].')' : ''}}</td>
 						</tr>
 						<tr>
-							<th class="red"><font color="white">BEST COMPLAINT HANDLING</font></th>
+							<th class="red"><font color="white">THE BEST OUTBOUND CALL CENTER</font></th>
+							<td>{{ $nilaiCategori6[0]==1 ? 'JUARA 1 (' .$resultCategori6[0].')' : ''}}</td>
+							<td>{{ $nilaiCategori6[1]==1 ? 'JUARA 1 (' .$resultCategori6[1].')' : ''}}</td>
+							<td>{{ $nilaiCategori6[2]==1 ? 'JUARA 1 (' .$resultCategori6[2].')' : ''}}</td>
+							<td>{{ $nilaiCategori6[3]==1 ? 'JUARA 1 (' .$resultCategori6[3].')' : ''}}</td>
+						</tr>						
+						<tr>
+							<th class="red"><font color="white">THE BEST COMPLAINT HANDLING</font></th>
 							<td>{{ $nilaiCategori8[0]==1 ? 'JUARA 1 (' .$resultCategori8[0].')' : ''}}</td>
 							<td>{{ $nilaiCategori8[1]==1 ? 'JUARA 1 (' .$resultCategori8[1].')' : ''}}</td>
 							<td>{{ $nilaiCategori8[2]==1 ? 'JUARA 1 (' .$resultCategori8[2].')' : ''}}</td>
 							<td>{{ $nilaiCategori8[3]==1 ? 'JUARA 1 (' .$resultCategori8[3].')' : ''}}</td>
 						</tr>
 						<tr>
-							<th class="red"><font color="white">BEST TRAINED</font></th>
+							<th class="red"><font color="white">THE BEST TRAINED</font></th>
 							<td>{{ $nilaiCategori9[0]==1 ? 'JUARA 1 (' .$resultCategori9[0].')' : ''}}</td>
 							<td>{{ $nilaiCategori9[1]==1 ? 'JUARA 1 (' .$resultCategori9[1].')' : ''}}</td>
 							<td>{{ $nilaiCategori9[2]==1 ? 'JUARA 1 (' .$resultCategori9[2].')' : ''}}</td>
